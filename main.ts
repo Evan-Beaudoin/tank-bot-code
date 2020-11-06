0e2a9f383a22f3ef10bd101309f27ab3cf59cf4d
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-11-06 14:50:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function scan_right () {
     robotbit.Servo(robotbit.Servos.S1, 0)
-    basic.pause(1000)
+    basic.pause(500)
     for (let index = 0; index < 1; index++) {
         right_distance = distance
     }
@@ -25,7 +25,7 @@ function turn_left () {
 }
 function scan_left () {
     robotbit.Servo(robotbit.Servos.S1, 180)
-    basic.pause(1000)
+    basic.pause(500)
     for (let index = 0; index < 1; index++) {
         left_distance = distance
     }
@@ -40,34 +40,63 @@ function turn_right () {
         robotbit.Motors.M1A,
         255
         )
-        basic.pause(500)
+        basic.pause(350)
         turn_right_value = 0
     }
     robotbit.MotorStopAll()
 }
+// Created by: Evan
+// Created on: November 6, 2020
+// 
+// This program controls an automonous car using a ping sensor
 let turn_right_value = 0
 let left_distance = 0
 let turn_left_value = 0
 let distance = 0
 let right_distance = 0
-let M1_SPEED = 120
+let M1_SPEED = 130
 let M2_SPEED = 255
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+// Main Code
+// 
 basic.forever(function () {
     straighten_out()
     basic.pause(500)
     if (distance < 30 && distance > 0) {
         robotbit.MotorStopAll()
+        basic.clearScreen()
+        strip.clear()
+        strip.show()
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        music.playTone(880, music.beat(BeatFraction.Double))
         basic.pause(1000)
         scan_left()
         basic.pause(1000)
         scan_right()
         basic.pause(1000)
         if (left_distance > right_distance) {
+            strip.clear()
+            strip.show()
+            strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+            strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+            strip.show()
+            basic.showArrow(ArrowNames.West)
             turn_left()
         } else {
+            strip.clear()
+            strip.show()
+            strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+            strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+            strip.show()
+            basic.showArrow(ArrowNames.East)
             turn_right()
         }
     } else {
+        strip.clear()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.showArrow(ArrowNames.North)
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         M1_SPEED,
