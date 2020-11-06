9e1f866e77f8d0ee8de673fe2d55f1f1a568e08d
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-11-06 12:49:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,43 @@
-let distance = 0
-basic.forever(function () {
-    if (distance < 10 && distance > 0) {
-        robotbit.MotorStopAll()
-    } else {
+function straighten_out () {
+    robotbit.Servo(robotbit.Servos.S1, 90)
+}
+function turn_left () {
+    while (turn_left_value == 1) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
-        255,
+        -152,
         robotbit.Motors.M2A,
         255
         )
+        basic.pause(400)
+        turn_left_value = 0
     }
+    robotbit.MotorStopAll()
+}
+function turn_right () {
+    while (turn_right_value == 1) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2A,
+        -152,
+        robotbit.Motors.M1A,
+        255
+        )
+        basic.pause(400)
+        turn_right_value = 0
+    }
+    robotbit.MotorStopAll()
+}
+let distance = 0
+let turn_right_value = 0
+let turn_left_value = 0
+let M1_SPEED = 120
+let M2_SPEED = 255
+turn_left_value = 1
+turn_right_value = 1
+basic.forever(function () {
+    straighten_out()
+    basic.pause(1000)
+    turn_right()
 })
 basic.forever(function () {
     distance = sonar.ping(
@@ -17,5 +45,4 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    basic.showString("" + (distance))
 })
